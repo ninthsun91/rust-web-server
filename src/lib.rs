6ad61fe8717e3f3e4416954a5a64bd3ff06dda67
Ray Kim <ninthsun91@gmail.com>
2024-05-19 01:28:12 +0900

    Create worker pool

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
-use std::fmt;
+use std::{fmt, process::id, thread};
 
-pub struct ThreadPool;
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -11,15 +13,20 @@ impl ThreadPool {
     /// - `new` will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
-        ThreadPool
+
+        let workers = create_workers(size);
+
+        ThreadPool { workers }
     }
 
     /// Create a new ThreadPool.
     /// 
     /// size: The number of threads in the pool. If the size is zero, return PoolCreationError.
     pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        let workers = create_workers(size);
+
         if size > 0 {
-            Ok(ThreadPool)
+            Ok(ThreadPool { workers })
         } else {
             Err(PoolCreationError)
         }
@@ -31,6 +38,27 @@ impl ThreadPool {
     {}
 }
 
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker { id, thread }
+    }
+}
+
+fn create_workers(size: usize) -> Vec<Worker> {
+    let mut workers = Vec::with_capacity(size);
+    for id in 0..size {
+        workers.push(Worker::new(id));
+    }
+
+    workers
+}
 pub struct PoolCreationError;
 
 impl fmt::Display for PoolCreationError {
